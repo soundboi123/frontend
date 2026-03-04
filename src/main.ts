a385eddf00c4d88782fd1dd9b6c9b5224e2ad767
wobbe-ubuntu <wobbeverbesselt@hotmail.com>
2026-03-04 22:54:51 +0100

    when will i be able to make this work??

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './main.css'
 
+import { registerSW } from 'virtual:pwa-register'
+
+registerSW({ immediate: true })
 createApp(App).mount('#app')
